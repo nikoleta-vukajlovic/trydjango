e02966fc0450486767f5ac4ef483c490a0859e85
Nikoleta Vukajlovic <44413453+nikoleta-vukajlovic@users.noreply.github.com>
2022-01-20 10:14:41 +0100

    Update forms.py

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from .models import Product
 
 
+
 class ProductForm(forms.ModelForm):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
     description = forms.CharField(
